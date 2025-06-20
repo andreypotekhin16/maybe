e87f1d302e80f7e8b650db5629a9a2a56300ecee
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-06-21 01:37:40 +0300

    vers.10

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,11 @@
 # main/admin.py
 from django.contrib import admin
-from .models import Service, CompanyProfile, OrbibolInfo, Feature, GameType # Добавили GameType
+from .models import Service, CompanyProfile, OrbibolInfo, Feature, GameType, Product, GalleryItem
 
 admin.site.register(Service)
 admin.site.register(CompanyProfile)
 admin.site.register(OrbibolInfo)
 admin.site.register(Feature)
-admin.site.register(GameType) # Зарегистрировали GameType
+admin.site.register(GameType)
+admin.site.register(Product)
+admin.site.register(GalleryItem)
