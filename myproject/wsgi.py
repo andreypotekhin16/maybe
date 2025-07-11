2787948c56b801517a55e10afda9363ec1a64453
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-11 15:42:48 +0300

    vers.fix.01

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+from django.conf import settings
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
