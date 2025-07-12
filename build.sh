5b4304b9ceab2d460792e8c3f605bb1ad16433a9
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 16:21:26 +0300

    Remove favicon link to allow build to pass

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
-# START OF FILE: build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Собираем статику, ИГНОРИРУЯ все папки, которые могут вызвать конфликт.
-# Это заставит сборку пройти успешно.
-python manage.py collectstatic --no-input --ignore cloudinary --ignore "main/site_assets" --ignore "site_assets"
+python manage.py collectstatic --no-input
 
 python manage.py migrate
 python manage.py createsuper
-
-# END OF FILE: build.sh
