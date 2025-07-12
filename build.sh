71f5cb9cb0b4b7d62aadf359465ca70f57c9c6e8
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 13:42:45 +0300

    Force ignore cloudinary static files during build

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
-# START OF FILE: build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Собираем статику, ИГНОРИРУЯ проблемную папку, чтобы сборка не падала
-python manage.py collectstatic --no-input --ignore "main/site_assets"
+# Собираем статику, ИГНОРИРУЯ все файлы из папки 'cloudinary', чтобы избежать конфликта
+python manage.py collectstatic --no-input --ignore cloudinary
 
 python manage.py migrate
 python manage.py createsuper
-
-# END OF FILE: build.sh```
