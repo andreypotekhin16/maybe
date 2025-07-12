0174b9e051152a2c9cfd266f243296cac4702355
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 13:51:14 +0300

    Final fix for all build and template issues

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
+# START OF FILE: build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Собираем статику, ИГНОРИРУЯ все файлы из папки 'cloudinary', чтобы избежать конфликта
-python manage.py collectstatic --no-input --ignore cloudinary
+# Собираем статику, ИГНОРИРУЯ проблемные папки. Это решит ошибку сборки.
+python manage.py collectstatic --no-input --ignore "cloudinary" --ignore "main/site_assets" --ignore "site_assets"
 
 python manage.py migrate
 python manage.py createsuper
+
+# END OF FILE: build.sh
