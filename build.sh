d056d41cb2e8388fef22b074ffd343b11d246573
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 17:15:32 +0300

    Final attempt to fix build by removing favicon link and ignoring static folders

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Игнорируем ВСЕ папки, которые могут вызвать конфликт, чтобы сборка прошла
-python manage.py collectstatic --no-input --ignore cloudinary --ignore "main/site_assets" --ignore "site_assets" --ignore "main/images"
+# Собираем статику, ИГНОРИРУЯ все папки, которые могут вызвать конфликт
+python manage.py collectstatic --no-input --ignore cloudinary --ignore "main/site_assets" --ignore "main/images"
 
 python manage.py migrate
 python manage.py createsuper
