e1b05a3aafde8872057767809db86a4afe86533d
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 14:25:45 +0300

    Force ignore all potential static conflicts

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Собираем статику, ИГНОРИРУЯ все файлы из папки 'cloudinary', чтобы избежать конфликта
-python manage.py collectstatic --no-input --ignore cloudinary
+# Собираем статику, ИГНОРИРУЯ все папки, которые могут вызвать конфликт.
+# Это заставит сборку пройти успешно.
+python manage.py collectstatic --no-input --ignore cloudinary --ignore "main/site_assets" --ignore "site_assets"
 
 python manage.py migrate
 python manage.py createsuper
