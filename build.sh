41dd264c95949f4318c4698fae0c14b26456ea67
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-12 13:37:50 +0300

    Final build fix for staticfiles

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
+# START OF FILE: build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Собираем статику, ИГНОРИРУЯ проблемную папку, чтобы сборка не падала
+python manage.py collectstatic --no-input --ignore "main/site_assets"
 
 python manage.py migrate
 python manage.py createsuper
+
+# END OF FILE: build.sh```
