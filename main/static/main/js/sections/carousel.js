785ee92d6b2ac2637d4b7b209f36c79eab2376e1
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-15 16:04:34 +0300

    карусель.рабочая.версия

--- main/static/main/js/sections/carousel.js
+++ main/static/main/js/sections/carousel.js
@@ -1,29 +1,26 @@
 // main/static/main/js/sections/carousel.js
 
-// Мы оставляем название функции, чтобы main.js мог ее вызвать
 export function setupSimpleCarousel() {
     
-    // Но внутри инициализируем Swiper с эффектом 'coverflow'
-    const swiper = new Swiper('.simple-carousel', { // <-- Указываем класс, который ищет main.js
+    const swiper = new Swiper('.simple-carousel', {
         
-        // --- Настройки для эффекта Coverflow ---
         effect: 'coverflow',
         grabCursor: true,
         centeredSlides: true,
-        slidesPerView: 'auto',
+        slidesPerView: 'auto', // 'auto' лучше всего подходит для coverflow
         loop: true,
         
+        // Немного меняем настройки для круглых слайдов
         coverflowEffect: {
             rotate: 0,
-            stretch: -20,
-            depth: 100,
-            modifier: 1.5,
+            stretch: 50,  // << Увеличиваем расстояние между слайдами
+            depth: 150, // << Увеличиваем глубину для объема
+            modifier: 1,
             slideShadows: false,
         },
 
-        // --- Навигация ---
         navigation: {
-            // Классы стрелок берем из нового HTML
+            // Пути к кнопкам не меняем, они остались прежними
             nextEl: '.carousel-wrapper .swiper-button-next',
             prevEl: '.carousel-wrapper .swiper-button-prev',
         },
