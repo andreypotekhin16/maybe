92d3ca746590b7fd9ff563186f15f576c80991d2
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-15 15:58:34 +0300

    игшдsfwацацццацц

--- main/static/main/js/sections/carousel.js
+++ main/static/main/js/sections/carousel.js
@@ -1,35 +1,31 @@
-// ---- НОВАЯ ВЕРСИЯ СКРИПТА от [текущее время] ----
-// ---- Если вы видите этот комментарий, файл обновился ----
+// main/static/main/js/sections/carousel.js
 
-// Функция для запуска нашей карусели
+// Мы оставляем название функции, чтобы main.js мог ее вызвать
 export function setupSimpleCarousel() {
     
-    // Выводим сообщение в консоль, чтобы убедиться, что наш скрипт работает
-    console.log("ЗАПУСК ИНИЦИАЛИЗАЦИИ НОВОЙ ВЕРСИИ КАРУСЕЛИ!");
+    // Но внутри инициализируем Swiper с эффектом 'coverflow'
+    const swiper = new Swiper('.simple-carousel', { // <-- Указываем класс, который ищет main.js
+        
+        // --- Настройки для эффекта Coverflow ---
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        
+        coverflowEffect: {
+            rotate: 0,
+            stretch: -20,
+            depth: 100,
+            modifier: 1.5,
+            slideShadows: false,
+        },
 
-    // Инициализируем Swiper
-    const carouselInstance = new Swiper('.simple-carousel', {
-        // Настройки карусели
-        
-        // 1. Поведение прокрутки
-        loop: true, // Бесконечный цикл
-        slidesPerView: 3, // Показывать по одному слайду
-        
-        // 2. Управление
+        // --- Навигация ---
         navigation: {
-            nextEl: '.my-awesome-carousel-wrapper .swiper-button-next',
-            prevEl: '.my-awesome-carousel-wrapper .swiper-button-prev',
-        },
-
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-        // 3. Автопрокрутка (для примера)
-        autoplay: {
-            delay: 5000, // Каждые 5 секунд
-            disableOnInteraction: false, // Не останавливать после ручного переключения
+            // Классы стрелок берем из нового HTML
+            nextEl: '.carousel-wrapper .swiper-button-next',
+            prevEl: '.carousel-wrapper .swiper-button-prev',
         },
     });
 }
