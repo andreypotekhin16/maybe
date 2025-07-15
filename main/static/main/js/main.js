b8bea0f6ada5ea9440fa0d0209af016c308fbc27
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-15 15:44:39 +0300

    игшдsfwefqfwwwgwfwfewfwfwfwацццв

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,14 +1,13 @@
 import { setupHeader } from './global/header.js';
 import { setupDynamicBackground } from './global/background.js';
-// Название импортируемой функции изменено
-import { setupSimpleCarousel } from './sections/carousel.js'; 
+import { initCarousel } from './sections/carousel.js'; 
 
 document.addEventListener('DOMContentLoaded', function() {
     setupHeader();
     setupDynamicBackground();
 
-    // Ищем контейнер с новым классом и вызываем новую функцию
-    if (document.querySelector('.simple-carousel')) {
-        setupSimpleCarousel();
+    // Ищем нашу карусель и запускаем ее
+    if (document.querySelector('.booking-carousel')) {
+        initCarousel();
     }
 });
