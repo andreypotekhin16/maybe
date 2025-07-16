411db528b54eb81aae9d351b44c769dcb59c7335
Andrey Potekhin <andreypotekhin16@gmail.com>
2025-07-16 16:23:02 +0300

    галерея.рабочая.версия.22

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,11 +3,5 @@
 from .models import Section
 
 def nav_sections(request):
-    """
-    Добавляет список активных секций для навигации в контекст всех шаблонов.
-    """
-    # Запрашиваем только активные секции и сразу сортируем их по полю 'order'
     sections_for_nav = Section.objects.filter(is_active=True).order_by('order')
-    
-    # Возвращаем словарь, который будет добавлен в контекст
     return {'nav_sections': sections_for_nav}
